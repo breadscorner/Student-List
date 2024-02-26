05e604c243e603d6bd1f80c74bf3473b2cd06df4
breadscorner <hello@brettgill.io>
2024-02-26 14:28:17 -0800

    switch to style file

--- Lab5App/Views/includes/header.php
+++ Lab5App/Views/includes/header.php
@@ -4,56 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student List</title>
-    <style>
-        body {
-            font-family: sans-serif;
-        }
-        h1, h2 {
-            text-align: center;
-        }
-        table {
-            border-collapse: collapse;
-            width: 50%;
-            margin: 20px auto;
-            border: 1px solid #ddd;
-        }
-        th {
-            color: navy;
-            text-transform: uppercase;
-        }
-        th, td {
-            padding: 10px;
-            text-align: left;
-            border-bottom: 1px solid #ddd; /* Add border bottom */
-        }
-        tr:nth-child(2n) {
-            background-color: rgb(200 200 255 / 0.5);
-        }
-        .create-form {
-            display: flex;
-            align-items: center;
-            width: fit-content; /* Set width to fit content */
-        }
-        .create-form input[type="text"] {
-            padding: 8px;
-            margin-right: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            width: auto;
-        }
-        .create-form input[type="submit"] {
-            padding: 8px 20px;
-            background-color: navy;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            margin-left: auto; /* Align button to the right */
-        }
-        .create-form input[type="submit"]:hover {
-            background-color: #001f3f;
-        }
-    </style>
+    <link rel="stylesheet" href="localhost/lab5/Lab5App/Styles/styles.css">
 </head>
 <body>
     <header>
